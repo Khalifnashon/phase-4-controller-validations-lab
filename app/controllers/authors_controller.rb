ca79676bf666d3691842d5b4808d741b8c15a8f3
+ <+>
2023-03-16 10:27:20 +0300

    completed lab

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,8 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # validation rescue_from 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +10,33 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
+  # PATCH /birds/:id
+  def update
+    author = find_author
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    author.update!(author_params)
+    render json: author
+  end
+
   private
-  
+  def find_author
+    Author.find(params[:id])
+  end
+
   def author_params
     params.permit(:email, :name)
   end
-  
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Author not found" }, status: :not_found
+  end
 end
